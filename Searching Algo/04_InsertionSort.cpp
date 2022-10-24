7d0f91437e092de5415cd700ed488ed3b366e74c
SamarthPaliwal03 <samarthpaliwal3@gmail.com>
2022-10-24 22:15:05 +0530

    insertion Sort

---
+++ Searching Algo/04_InsertionSort.cpp
@@ -0,0 +1,32 @@
+//card wala eg ,here we dont swap we just shift it
+
+#include<iostream>
+using namespace std;
+
+void InsertionSort(int arr[],int size){
+    for (int i = 1; i <size; i++)
+    {
+        int temp=arr[i];
+        int j = i-1; //did so that we can use outside for loop of j 
+        for ( ; j>=0; j--)
+        {
+            if (arr[j]>temp)
+            {
+                arr[j+1]=arr[j]; //shifting
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1]=temp; //
+    } 
+}
+
+int main()
+{
+    int arr[5]={8,7,6,5,99};
+    InsertionSort(arr,5);
+    for (int i = 0; i < 5; i++) {
+    cout << arr[i] << "  ";
+    }
+}
